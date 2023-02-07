d4d9bacca4ada6f2f48eb00c3a8c9d6b6b646e13
erwan <erwan@DESKTOP-O25RGGR>
2023-02-07 13:50:58 +0100

    PremierCommit

---
+++ SLAM1_TPpre1_preProRPjet/src/main/java/fr/eni/slam1_tppre1_preprorpjet/SLAM1_TPpre1_preProRPjet.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package fr.eni.slam1_tppre1_preprorpjet;
+
+/**
+ *
+ * @author erwan
+ */
+public class SLAM1_TPpre1_preProRPjet {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
